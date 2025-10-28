b9476a0798e774ebe5fbadcee5cd4cef02cb1863
Isaacsantos-13 <isaacmendessantos98@gmail.com>
2025-10-27 21:20:24 -0300

    Add files via upload

---
+++ exercicios-vazios-main/exercicios-vazios-main/ex001/script.js
@@ -0,0 +1,4 @@
+let nome = (prompt("Digite seu nome"))
+let sobrenome = (prompt("digite seu sobrenome"))
+let nomeCompleto = (prompt("digite seu nome completo"))
+alert (`Óla, meu nome é ${nome} e meu sobrenome é ${sobrenome}`)
